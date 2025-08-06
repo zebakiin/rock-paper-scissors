ebf105bdee4bc6fe3034d81b8aa2e05a2283d549
Zeba <linorufaro@gmail.com>
2025-08-06 15:41:03 +0200

    Add score increments and expression

--- index.js
+++ index.js
@@ -56,13 +56,14 @@ let computerScore = 0;
 //
 if (humanChoice == "rock" && computerChoice == "scissors" ||
      humanChoice == "scissors" && computerChoice == "paper" ||
-      humanChoice == "paper" && computerChoice == "rock"
-) {console.log ("You are the Vitkor! Stand proud, but dont let your valor get the better of you...")}
+      humanChoice == "paper" && computerChoice == "rock")
+       {console.log ("You are the Vitkor! Stand proud, but dont let your valor get the better of you..."); humanScore++; console.log ("You have:", humanScore)}
 
 else if (humanChoice == "rock" && computerChoice == "paper" ||
      humanChoice == "scissors" && computerChoice == "rock" ||
       humanChoice == "paper" && computerChoice == "scissors"
-) {console.log ("You have lost this one.. Dont fret for you might still stand victorius")}
+) {console.log ("You have lost this one.. Dont fret for you might still stand victorius"); computerScore++; console.log ("It has:", computerScore)
+}
 
  else if (humanChoice == computerChoice) {console.log ("No Points for No one")}
     
