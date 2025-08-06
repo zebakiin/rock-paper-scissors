8b1e1400487bf7163574bc7ad791bc9e97a1dfeb
Zeba <linorufaro@gmail.com>
2025-08-06 23:48:05 +0200

    Move score variables into Playround

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
 console.log("Hello Team!")
 
 
-
+//Makes a Random Choice and gives returns rock paper or scissors
 function getComputerChoice () {
     let result
     result = Math.random()
 
     if (result >= 2/3) {
-        console.log ("Rock!")
+       
         return "rock"
     }
     else if (result <= 1/3) { 
-        console.log ("Paper!")
+
         return "paper"
     }
-    else console.log ("Scissors!")
+    else 
     return "scissors"
 }
 
+//Asks the Human for their answer 
 function askHuman () {answer = prompt("1: Rock 2: Paper 3: Scissors!").toLowerCase();
                         return answer;
 }
 
 askHuman();
 
+//Accepts the answer and returns rock paper or scissors
 function getHumanChoice () {
     
     if (answer == "rock" || answer == 1 ) {
-        console.log ("Rock!")
+       
         return "rock"
     }
 
     else if (answer == "paper"|| answer == 2) {
-        console.log ("Paper!")
+      
         return "paper"
     }
 
     else if (answer == "scissors"|| answer == 3) {
-        console.log ("Scissors!")
+        
         return "scissors"
     }
     
-    else {console.log ("Hey that wasnt an option >:(")
+    else
         return "not taken"
      }
-    };
-function getChoices() {askHuman(), getComputerChoice(),  getHumanChoice()}
+    ;
+// ouputs the answers for the viewer
+    function answers () {
+console.log ("You have chosen..:", humanChoice)
+console.log ("It chose..:", computerChoice)}
 
-let humanScore = 0; 
-let computerScore = 0;
+//calls all these above
+function getChoices() {askHuman(), humanChoice, computerChoice, answers()}
+
   
 
-  
+
    const humanChoice = getHumanChoice(); 
   const computerChoice = getComputerChoice();
+
+ 
   
   const noWin = (humanChoice == computerChoice);
 
@@ -66,21 +74,38 @@ let computerScore = 0;
   
 
  
-getChoices();
+
 
 
 
   function playGame () {
+
         function playRound () {
+            getChoices();
+
+
+        //sets both scores to 0
+
+            let humanScore = 0; 
+            let computerScore = 0;
+
+        //incriment the scores based on who wins
+        function scoreKeeper () {
+            if (humanWins) {humanScore++}
+            else if (computerWins) {computerScore++}
+        }
     //If I choose rock and the other person chooses rock, nothing happens. IF the other person chooses Paper, I lose. If the other person chooses Scissors, I win
     // Rock beats scissors ; Scissors beats Paper ; Paper beats Rock
-    if (humanWins) {{console.log ("You are the Vitkor! Stand proud, but dont let your valor get the better of you..."); humanScore++; console.log ("You have:", humanScore); return humanScore++}
+    if (humanWins) {{console.log ("You are the Vitkor! Stand proud, but dont let your valor get the better of you..."); scoreKeeper(); console.log ("You have:", humanScore,);}
     }
-    else if(computerWins) {{console.log ("You have lost this one.. Dont fret for you might still stand victorius"); computerScore++; console.log ("It has:", computerScore); return computerScore++}
+    else if(computerWins) {{console.log ("You have lost this one.. Dont fret for you might still stand victorius"); scoreKeeper(); console.log ("It has:", computerScore,);}
     }
     else if (noWin) {console.log ("No Points for No one")}
     };
+
+
     playRound();
+            
   };
   
   playGame()
