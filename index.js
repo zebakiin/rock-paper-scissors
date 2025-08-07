9db8855e3e20ba029b2fe1ce586d305ef173e719
Zeba <linorufaro@gmail.com>
2025-08-08 00:37:12 +0200

    Restructure the logic simplfy & redone Made playRound function include the necacarry logic. Created Playround paramters and arguments instead of having the logic as a constant.

--- index.js
+++ index.js
@@ -18,16 +18,10 @@ function getComputerChoice () {
     return "scissors"
 }
 
-//Asks the Human for their answer 
-function askHuman () {answer = prompt("1: Rock 2: Paper 3: Scissors!").toLowerCase();
-                        return answer;
-}
-
-askHuman();
-
-//Accepts the answer and returns rock paper or scissors
 function getHumanChoice () {
-    
+
+    answer = prompt("1: Rock 2: Paper 3: Scissors!").toLowerCase()
+
     if (answer == "rock" || answer == 1 ) {
        
         return "rock"
@@ -45,72 +39,38 @@ function getHumanChoice () {
     
     else
         return "not taken"
-     }
-    ;
-// ouputs the answers for the viewer
-    function answers () {
-console.log ("You have chosen..:", humanChoice)
-console.log ("It chose..:", computerChoice)}
+     };
+    
+
 
 //calls all these above
-function getChoices() {askHuman(), humanChoice, computerChoice, answers()}
 
-  
+let humanScore = 0
+let computerScore = 0
 
 
-   const humanChoice = getHumanChoice(); 
-  const computerChoice = getComputerChoice();
 
- 
-  
-  const noWin = (humanChoice == computerChoice);
+function playRound(humanChoice, computerChoice) {
 
-  const humanWins = (humanChoice == "rock" && computerChoice == "scissors" ||
+   if (humanChoice == "rock" && computerChoice == "scissors" ||
      humanChoice == "scissors" && computerChoice == "paper" ||
-      humanChoice == "paper" && computerChoice == "rock");
-  const computerWins =(humanChoice == "rock" && computerChoice == "paper" ||
+      humanChoice == "paper" && computerChoice == "rock") {console.log ("You are Viktor! You chose:", humanChoice, "which beats:", computerChoice, ++humanScore)}
+
+        else if ((humanChoice == "rock" && computerChoice == "paper" ||
      humanChoice == "scissors" && computerChoice == "rock" ||
-      humanChoice == "paper" && computerChoice == "scissors");
+    humanChoice == "paper" && computerChoice == "scissors")) {console.log ("You lost this time You chose:", humanChoice, "which loses to:", computerChoice, ++computerScore)}
+
+    else {console.log ("A standstil - You chose:", humanChoice, "It chose:", computerChoice)}
+}
+     const humanSelection = getHumanChoice(); 
+  const computerSelection = getComputerChoice();
+
+
+  playRound(humanSelection, computerSelection)
+
   
 
  
-
-
-
-
-  function playGame () {
-
-        function playRound () {
-            getChoices();
-
-
-        //sets both scores to 0
-
-            let humanScore = 0; 
-            let computerScore = 0;
-
-        //incriment the scores based on who wins
-        function scoreKeeper () {
-            if (humanWins) {humanScore++}
-            else if (computerWins) {computerScore++}
-        }
-    //If I choose rock and the other person chooses rock, nothing happens. IF the other person chooses Paper, I lose. If the other person chooses Scissors, I win
-    // Rock beats scissors ; Scissors beats Paper ; Paper beats Rock
-    if (humanWins) {{console.log ("You are the Vitkor! Stand proud, but dont let your valor get the better of you..."); scoreKeeper(); console.log ("You have:", humanScore,);}
-    }
-    else if(computerWins) {{console.log ("You have lost this one.. Dont fret for you might still stand victorius"); scoreKeeper(); console.log ("It has:", computerScore,);}
-    }
-    else if (noWin) {console.log ("No Points for No one")}
-    };
-
-
-    playRound();
-            
-  };
-  
-  playGame()
-  
-
 
 
  
